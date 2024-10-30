897d61fccc06b3f49a50e98c040744a5bb4297dc
keven.ferraz <keven.ferraz@mercadolivre.com>
2024-10-30 23:34:54 +0000

    ATUALIZAÇÃO DO RODAPÉ

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -11,41 +11,20 @@ const Footer = () => {
                 </div>
                 <div className={style.information}>
                     <ol>
-                        <h3>Conteúdo</h3>
-                        <li><a href="https://github.com/Kevenferraz39">Atividades</a></li>
-                        <li><a href="https://github.com/Kevenferraz39">Videos</a></li>
-                        <li><a href="https://github.com/Kevenferraz39">Conteudo</a></li>
-                        <li><a href="https://github.com/Kevenferraz39">Documentacao</a></li>
-                        <li><a href="https://github.com/Kevenferraz39">Tags</a></li>
+                        <h3>Gestão</h3>
+                        <li><a href="https://lookerstudio.google.com/u/0/reporting/addc1e45-1c23-4654-9b8e-c4320389d778/page/p_s3d5oqq09c">Setup Time</a></li>
+                        <li><a href="https://lookerstudio.google.com/reporting/adee4ffc-6a1f-4984-a99e-dc754f782d42">Meal Accuraycy</a></li>
+                        <li><a href="https://lookerstudio.google.com/u/0/reporting/2e395b9e-8d62-488e-b24a-dd3ce2adaef8/page/p_960xu7q98c">Monitor de Eficiência</a></li>
+                        <li><a href="https://lookerstudio.google.com/u/0/reporting/2e395b9e-8d62-488e-b24a-dd3ce2adaef8/page/p_ly9lunpzld">Rep Evolution</a></li>
                     </ol>
 
                     <ol>
-                        <h3>Conteúdo</h3>
-                        <li><a href="https://github.com/Kevenferraz39">Atividades</a></li>
-                        <li><a href="https://github.com/Kevenferraz39">Videos</a></li>
-                        <li><a href="https://github.com/Kevenferraz39">Conteudo</a></li>
-                        <li><a href="https://github.com/Kevenferraz39">Documentacao</a></li>
-                        <li><a href="https://github.com/Kevenferraz39">Tags</a></li>
+                        <h3>Visão Geral</h3>
+                        <li><a href="https://lookerstudio.google.com/reporting/92a1f5fe-a331-4f37-9d13-37d0e20f2137">SLA Processos</a></li>
+                        <li><a href="https://lookerstudio.google.com/reporting/e73a5633-90fe-4914-af08-9ff9d9b6b13e">Aging de Orders</a></li>
+                        <li><a href="https://lookerstudio.google.com/reporting/711487b7-cd31-4cb1-85d1-9ea894e077af/page/tEnnC">Stock Audit</a></li>
+                        <li><a href="https://sites.google.com/mercadolivre.com/idea-sp10/idea/formul%C3%A1rio">Solicitação de Demanda</a></li>
                     </ol>
-
-                    <ol>
-                        <h3>Conteúdo</h3>
-                        <li><a href="https://github.com/Kevenferraz39">Atividades</a></li>
-                        <li><a href="https://github.com/Kevenferraz39">Videos</a></li>
-                        <li><a href="https://github.com/Kevenferraz39">Conteudo</a></li>
-                        <li><a href="https://github.com/Kevenferraz39">Documentacao</a></li>
-                        <li><a href="https://github.com/Kevenferraz39">Tags</a></li>
-                    </ol>
-
-                    <ol>
-                        <h3>Conteúdo</h3>
-                        <li><a href="https://github.com/Kevenferraz39">Atividades</a></li>
-                        <li><a href="https://github.com/Kevenferraz39">Videos</a></li>
-                        <li><a href="https://github.com/Kevenferraz39">Conteudo</a></li>
-                        <li><a href="https://github.com/Kevenferraz39">Documentacao</a></li>
-                        <li><a href="https://github.com/Kevenferraz39">Tags</a></li>
-                    </ol>
-
                 </div>
             </div>
             <br/>
--- src/components/pages/sobre/index.jsx
+++ src/components/pages/sobre/index.jsx
@@ -2,7 +2,6 @@ import { Helmet } from 'react-helmet';
 import '../../../estilo.css'; // Corrigido para garantir que o CSS seja importado corretamente
 import Navbar from '../../Navbar';
 import Footer from '../../Footer';
-import Main from '../../Main'
 const Sobre = () => {
     return(
         <>
@@ -10,7 +9,7 @@ const Sobre = () => {
                 <title>Sobre</title>
             </Helmet>
             <main>
-                <img src="" alt="" />
+                <center><h1 className='emconstruction'>ESTÁ PÁGINA ESTÁ EM DESENVOLVIMENTO !</h1></center>
             </main>
             <Footer/>
         <Navbar/>
